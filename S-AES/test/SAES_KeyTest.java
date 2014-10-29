e845d3e9fa8e3f349238bd70e083cd59be00249c
Noel Niles <shakabra321@gmail.com>
2014-10-29 13:06:51 -1000

    Add GNU license and fix some comment formatting.

--- S-AES/test/SAES_KeyTest.java
+++ S-AES/test/SAES_KeyTest.java
@@ -1,8 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/** Copyright 2014 Noel Niles
+ * 
+ * This file is part of SAES
+ *
+ * S-AES is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ *along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -10,9 +22,16 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
+/** S-AES_KeyTest
+ * 
+ * Simple implementation of Advanced Encryption Standard. The main difference
+ * between standard AES and S-AES is that AES does 10/12/14 rounds while S-AES 
+ * only does 3 rounds. Also the block size for S-AES is 16-bits compared to AES
+ * which uses 128-bit block sizes.
  *
- * @author noel
+ * @author Noel Niles
+ * @version 1.0
+ * @since 2014-10-28
  */
 public class SAES_KeyTest {
     
